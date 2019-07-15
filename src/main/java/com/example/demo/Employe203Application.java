259efc2640f5dbea0f1b5af0c757f118177337ac
AshuAshenafi <ashuashenafi@gmail.com>
2019-07-15 22:07:29 +0300

    exercise 203

---
+++ src/main/java/com/example/demo/Employe203Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employe203Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Employe203Application.class, args);
+    }
+
+}
